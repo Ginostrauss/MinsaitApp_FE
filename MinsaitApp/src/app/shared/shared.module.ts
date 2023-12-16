7ba82e9703ea43869be9ec5cc262bd05bf8df1b8
Gianluca Mazzilli <gianluca.mazzilli.external@enel.com>
2023-12-16 12:58:33 +0100

    BOOTSTRAP STYLE

--- MinsaitApp/src/app/shared/shared.module.ts
+++ MinsaitApp/src/app/shared/shared.module.ts
@@ -1,14 +1,6 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {MatToolbarModule} from "@angular/material/toolbar";
-import {MatButtonModule} from "@angular/material/button";
-import {MatFormFieldModule} from "@angular/material/form-field";
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatIconModule} from '@angular/material/icon';
-import {MatInputModule} from "@angular/material/input";
-import {MatAutocompleteModule} from '@angular/material/autocomplete';
-import {MatCardModule} from '@angular/material/card';
-import {MatDividerModule} from '@angular/material/divider';
 import {CdkAccordionModule} from '@angular/cdk/accordion';
 import {RouterModule} from "@angular/router";
 import { NavbarComponent } from './navbar/navbar.component';
@@ -17,27 +9,16 @@ import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-
-
     NavbarComponent
   ],
   imports: [
     CommonModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatFormFieldModule,
     FormsModule,
     ReactiveFormsModule,
-    MatIconModule,
-    MatInputModule,
-    MatAutocompleteModule,
-    MatCardModule,
-    MatDividerModule,
     CdkAccordionModule,
     RouterModule
   ],
   exports: [
-
     RouterModule,
     NavbarComponent
   ]
